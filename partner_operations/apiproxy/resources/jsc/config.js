f29b0e668adadf35fa3fc6d751f3740f10000bf3
shridharnagaral <shridhar.nagaral@honeywell.com>
2016-05-09 20:02:23 +0530

    This is new proxy for Partner web app.   proxy name : partner_operation   : Add-Edit-Search-get-delete-getassets-deleteassets
    This is new proxy for Partner web app.   proxy name : partner_operation   : Add-Edit-Search-get-delete-getassets-deleteassets

---
+++ partner_operations/apiproxy/resources/jsc/config.js
@@ -0,0 +1,11 @@
+var basepath = "connectedhome-dev.apigee.net";
+var hch_baas = "api.usergrid.com";
+var partner_baas = "shridharnagaral1/sandbox/partnerdata";
+var partner_assets_baas = "shridharnagaral1/sandbox/assets";
+
+
+
+context.setVariable('basepath',basepath);
+context.setVariable('hch_baas',hch_baas);
+context.setVariable('partner_baas',partner_baas);
+context.setVariable('partner_assets_baas',partner_assets_baas);
